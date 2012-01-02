29d978d87af62256b69b810f3c99b16ede9b5ce5
Adam Brodziak <adam@globalsportsmedia.com>
2012-01-02 12:42:23 +0100

    Extract formatting messge to separate method.

--- src/DoctrineTelemetry/Logger/LazyLoadingLogger.php
+++ src/DoctrineTelemetry/Logger/LazyLoadingLogger.php
@@ -20,20 +20,27 @@ class LazyLoadingLogger implements SQLLogger
     public function startQuery($sql, array $params = null, array $types = null)
     {
         foreach ($this->_matcher->match(debug_backtrace()) as $match) {
-            error_log(
-                sprintf(
-                    '%s:%d, %s%s%s(): Lazy loading detected of %s (from %s->%s)',
-                    $match->getOrigin('file'),
-                    $match->getOrigin('line'),
-                    $match->getOrigin('class'),
-                    $match->getOrigin('type'),
-                    $match->getOrigin('function'),
-                    $match->getClassMapping('targetEntity'),
-                    $match->getClassMapping('sourceEntity'),
-                    $match->getClassMapping('fieldName')
-                )
-            );
+            error_log(self::formatMessage($match));
         }
+    }
+
+    /**
+     * @param \DoctrineTelemetry\Backtrace\Query\Match $match
+     */
+    protected static function formatMessage($match) {
+        return (
+            sprintf(
+                '%s:%d, %s%s%s(): Lazy loading detected of %s (from %s->%s)',
+                $match->getOrigin('file'),
+                $match->getOrigin('line'),
+                $match->getOrigin('class'),
+                $match->getOrigin('type'),
+                $match->getOrigin('function'),
+                $match->getClassMapping('targetEntity'),
+                $match->getClassMapping('sourceEntity'),
+                $match->getClassMapping('fieldName')
+            )
+        );
     }
 
     public function stopQuery()
